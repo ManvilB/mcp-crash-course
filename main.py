e8d8cc0d776263a1bceb9826f7d41dbce503ad15
ManvilB <manvilb@gmail.com>
2026-07-13 16:50:53 -0400

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import asyncio
 
 from dotenv import load_dotenv
-from langchain.agents import create_agent
+from langgraph.prebuilt import create_react_agent
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
+from langchain_core.messages import HumanMessage
 
 load_dotenv()
 
@@ -17,7 +18,16 @@ stdio_server_params = StdioServerParameters(
 )
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+
+            agent = create_react_agent(llm, tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="WHat is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
