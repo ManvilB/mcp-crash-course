bdfe72590201e6b4638595aedcce7f2901560157
ManvilB <manvilb@gmail.com>
2026-07-13 16:01:55 -0400

    langchain client

--- servers/math_server.py
+++ servers/math_server.py
@@ -12,5 +12,5 @@ def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
     return a * b
 
-if __name__ == "main":
+if __name__ == "__main__":
     mcp.run(transport="stdio")
--- servers/weather_server.py
+++ servers/weather_server.py
@@ -8,5 +8,5 @@ async def get_weather(location: str) -> str:
     """Get weather for location."""
     return "Hot as hell"
 
-if __name__ == "main":
+if __name__ == "__main__":
     mcp.run(transport="sse")
